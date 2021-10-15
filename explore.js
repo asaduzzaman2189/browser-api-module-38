4e2b7af6ad9f12985825c84db50bbb352ee99c8a
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-10-15 16:39:56 +0600

    38-1 Module Overview, BOM vs DOM, Browser API

---
+++ explore.js
@@ -0,0 +1,17 @@
+const name = 15;
+const student = {
+  name: "jolil",
+  friend: "borsha",
+  job: "business",
+  makeMovie: function (name) {
+    console.log(name);
+  },
+};
+student.makeMovie("Din raat");
+
+// browser এর সাথে javascript এর সংযোগ যদি না থাকে তাহলে document fetch ইত্যাদি কাজ করবে না। 
+
+// BOM- Browser Object Model 
+
+// document.getElementById("name");
+
