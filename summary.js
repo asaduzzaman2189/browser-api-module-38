adeba5797fe0842512fa5b38052a3a6d253776a1
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-10-25 12:24:45 +0600

    38-9 Module Summary, ContentEditable, chrome restart

---
+++ summary.js
@@ -0,0 +1,18 @@
+// document,fetch normal plain-js file এ ব্যবহার করা যাবে না, শুধু ব্রাউজারের মধ্যে ব্যবহার করা যাবে।
+
+alert('');
+confirm('are you single?');
+prompt('name');
+
+//location.reload()
+//location.href=''
+//location.hash=
+//http://127.0.0.1:5500/shopping.html
+
+//history.back()
+//history.go()
+
+//cookies
+
+// sessionStorage vs localStorage
+//cookies vs sessionStorage vs localStorage
